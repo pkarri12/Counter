66663a4527873bd09aa85e3180098e15fad6b183
Devipriya <pkarri@gmail.com>
2020-03-07 17:31:25 -0600

    Updates

---
+++ precache-manifest.f06f200f58eb93c16ead311b51a48fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7bd477484288e395c6a308437f44d0",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "5a24f510673163772c61",
+    "url": "/Counter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7e78cdb70041e93146ec",
+    "url": "/Counter/static/js/2.b0973b82.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Counter/static/js/2.b0973b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a24f510673163772c61",
+    "url": "/Counter/static/js/main.f22f76c2.chunk.js"
+  },
+  {
+    "revision": "6d89d126b16e49e0e236",
+    "url": "/Counter/static/js/runtime-main.30e1db42.js"
+  }
+]);
